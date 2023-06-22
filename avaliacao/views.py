eac2ec20534ddb6e9d0ff61f69e571bc6c69d346
José Edston <103543263+Jose-Edston@users.noreply.github.com>
2023-06-22 18:57:08 -0300

    first api

---
+++ avaliacao/views.py
@@ -0,0 +1,43 @@
+from rest_framework.views import APIView
+from rest_framework import status, generics
+from .models import *
+from .serializer import *
+from rest_framework.response import Response
+
+
+class AlunoAPIView(generics.ListCreateAPIView):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+
+######### Metodo Tradicional ou Primeiro Metodo que o Caio fez ###################
+# class AlunoAPIView(APIView):
+#     def get(self, request):
+#         alunos = Aluno.objects.all()
+#         serializer = AlunoSerializer(alunos, many=True)
+#         return Response(serializer.data)
+#     def post(self, request):
+#         serializer = AlunoSerializer(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             # o "Raise_Exception" ele já faz o retorno (que seria nosso "else")
+#             sertializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class AvaliacaoAPIView(APIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+
+######### Metodo Tradicional ou Primeiro Metodo que o Caio fez ###################
+# class AvaliacaoAPIView(APIView):
+#     def get(self, request):
+#         avaliacoes = Avaliacao.objects.all()
+#         serializer = AvaliacaoSerializer(avaliacoes, many=True)
+#         return Response(serializer.data)
+#     def post(self, request):
+#         serializer = AvaliacaoSerializer(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
